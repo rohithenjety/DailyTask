d00e484ec818876a979e3ff1fae45ba5624990d0
rohithenjety <rohithenjety@gmail.com>
2024-09-09 16:43:24 +0530

    update

--- mergingsortedArray.java
+++ mergingsortedArray.java
@@ -41,13 +41,14 @@ class Rohith{
         else{
             arr3[k++]=arr2[j++];
         }
+    }
         while(i< arr1.length){
             arr3[k++]=arr1[i++];
         }
         while(j< arr2.length){
-            arr3[k++]=arr1[j++];
+            arr3[k++]=arr2[j++];
         }
-       }
+       
 
 
     }
